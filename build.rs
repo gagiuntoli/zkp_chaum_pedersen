0f12c520d98e7b8c8588f69807e8fb9e790fef0f
Guido Giuntoli <gagiuntoli@gmail.com>
2023-03-28 07:51:30 +0200

    Build gRPC interface for Rust using Tonic

---
+++ build.rs
@@ -0,0 +1,17 @@
+/// The code for implementing the Rust types by reading the protocol description
+/// was taken from:
+/// https://betterprogramming.pub/building-a-grpc-server-with-rust-be2c52f0860e
+///
+// use std::{env, path::PathBuf};
+
+fn main() {
+    let proto_file = "./proto/zkp_auth.proto";
+
+    tonic_build::configure()
+        .build_server(true)
+        .out_dir("./src")
+        .compile(&[proto_file], &["."])
+        .unwrap_or_else(|e| panic!("protobuf compile error: {}", e));
+
+    println!("cargo:rerun-if-changed={}", proto_file);
+}
