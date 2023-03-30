571e5665061388351846e9b13c362b4fe4ec0cd3
Guido Giuntoli <gagiuntoli@gmail.com>
2023-03-30 21:31:56 +0200

    Refactor RegisterResponse

--- src/server/main.rs
+++ src/server/main.rs
@@ -3,8 +3,9 @@ use std::collections::HashMap;
 use std::sync::Mutex;
 use num_bigint::BigUint;
 use std::cell::RefCell;
+use num::traits::Zero;
 
-use chaum_pedersen_zkp::{Point};
+use chaum_pedersen_zkp::{Point, get_random_number};
 
 pub mod zkp_auth {
     include!("../zkp_auth.rs");
@@ -30,10 +31,19 @@ pub struct AuthImpl {
 }
 
 #[derive(Debug, Clone)]
+pub struct InfoRegister {
+    pub user: String,
+    pub y1: Point,
+    pub y2: Point,
+}
+
+#[derive(Debug, Clone)]
 pub struct UserInfo {
     pub user: String,
     pub y1: Point,
     pub y2: Point,
+    pub r1: Point,
+    pub r2: Point,
 }
 
 #[tonic::async_trait]
@@ -42,25 +52,35 @@ impl Auth for AuthImpl {
         &self,
         request: Request<RegisterRequest>,
     ) -> Result<Response<RegisterResponse>, Status> {
-        println!("Request from {:?}", request.remote_addr());
+        //println!("Request from {:?}", request.remote_addr());
         println!("Request: {:?}", request);
 
-        let incoming_user = match (request.into_inner(), &self.group) {
-            (RegisterRequest { user, y1, y2 }, Group::Scalar) => UserInfo {
-                user,
-                y1: Point::deserialize_into_scalar(y1),
-                y2: Point::deserialize_into_scalar(y2),
-            },
-            _ => panic!("Register request is incompatible"),
-        };
-
+        let register_request = request.into_inner();
         let response = RegisterResponse {};
 
         // we add a new UserInfo if the id is new, we update y1 & y2 if `id` already exists.
         println!("Registry: {:?}", self.registry);
 
+        let user_info = match &self.group {
+            Group::Scalar => UserInfo {
+                user: register_request.user.clone(),
+                y1: Point::deserialize_into_scalar(register_request.y1),
+                y2: Point::deserialize_into_scalar(register_request.y2),
+                r1: Point::Scalar(BigUint::zero()),
+                r2: Point::Scalar(BigUint::zero()),
+            },
+            Group::EllipticCurve => UserInfo {
+                user: register_request.user.clone(),
+                y1: Point::deserialize_into_ecpoint(register_request.y1),
+                y2: Point::deserialize_into_ecpoint(register_request.y2),
+                r1: Point::Scalar(BigUint::zero()),
+                r2: Point::Scalar(BigUint::zero()),
+            },
+        };
+
         let registry = &mut *self.registry.lock().unwrap();
-        registry.insert(incoming_user.user.clone(), incoming_user);
+        registry.insert(register_request.user, user_info);
+
         println!("Registry: {:?}", self.registry);
 
         Ok(Response::new(response))
@@ -77,7 +97,12 @@ impl Auth for AuthImpl {
         &self,
         request: Request<AuthenticationChallengeRequest>,
     ) -> Result<Response<AuthenticationChallengeResponse>, Status> {
-        todo!()
+        let response = AuthenticationChallengeResponse {
+            auth_id: String::from("a"),
+            c: get_random_number::<32>().to_bytes_be(),
+        };
+
+        Ok(Response::new(response))
     }
 }
 
